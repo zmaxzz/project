9ea58d246fc07f66a4532f36a24c16543ca4dfbe
zmaxzz <114291983+zmaxzz@users.noreply.github.com>
2023-05-17 12:00:02 +0300

    Update Zd11.java

--- Задание №11/Zd11.java
+++ Задание №11/Zd11.java
@@ -1,13 +1,25 @@
-import java.util.Scanner;
-
 public class Zd11 {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String x = scanner.nextLine();
-        String[] chars = new String[] {"а","е","я","ё","у","и","ю","э","ы","о"};
-        for(int i = 0; i < chars.length; i++){
-            x = x.replace(chars[i],"");
+    private static final char [] mass = {'а', 'у', 'о' ,'ы','э','я','ю','ё','и','е'};
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Введите строку");
+        String x = scanner1.nextLine();
+        String y = "";
+        int z = 0;
+        char checkLetter;
+        for (int i = 0; i < x.length(); i++) {
+            z = 0;
+            checkLetter = x.charAt(i);
+            for (int j = 0; j < mass.length; j++) {
+                if (checkLetter == mass[j]) {
+                    z++;
+                    break;
+                }
+            }
+            if (z == 0) {
+                y = y + checkLetter;
+            }
         }
-        System.out.println(x);
+        System.out.println(y);
     }
 }
